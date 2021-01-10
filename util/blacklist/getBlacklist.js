651126564bfd7cc2c67d9a76ecce40c916d9cfa8
Chris Troutner <chris.troutner@gmail.com>
2021-01-10 08:30:49 -0800

    fix(blacklist): Adding util app for retrieving all blacklist models

---
+++ util/blacklist/getBlacklist.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const config = require('../../config')
+
+const Blacklist = require('../../src/models/blacklist')
+
+async function getUsers () {
+  // Connect to the Mongo Database.
+  mongoose.Promise = global.Promise
+  mongoose.set('useCreateIndex', true) // Stop deprecation warning.
+  await mongoose.connect(config.database, { useNewUrlParser: true })
+
+  const blacklist = await Blacklist.find({})
+  console.log(`blacklist: ${JSON.stringify(blacklist, null, 2)}`)
+
+  mongoose.connection.close()
+}
+getUsers()
